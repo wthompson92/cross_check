6e1309986d8e3d152240db0e8e8d2ecf903782c9
username <wnt925@gmail.com>
2019-05-20 19:25:45 -0600

    Add test for fan methods

--- test/league_test.rb
+++ test/league_test.rb
@@ -29,7 +29,43 @@ class LeaugeTest < Minitest:: Test
     actual = @league.best_defense
     assert_equal expected, actual
     expected = string
-    actual = @league.worst_defense 
+    actual = @league.worst_defense
     assert_equal expected, actual
   end
+
+  def test_highest_scoring_methods
+    expected = string
+    actual = @league.highest_scoring_visitor
+    assert_equal expected, actual
+
+    expected = string
+    actual = @leauge.highest_scoring_home_team
+    assert_equal expected, actual
+  end
+
+  def test_lowest_scoring_methods
+    expected = string
+    actual = @league.lowest_scoring_visitor
+    assert_equal expected, actual
+
+    expected = string
+    actual = @league.lowest_home_team
+    assert_equal expected, actual
+  end
+
+  def test_league_winningest_team
+    actual = string
+    expected = @league.winningest_team
+    assert_equal expected, actual
+  end
+
+  def test_fan_methods
+    expected = string
+    actual = @league.best_fans
+    assert_equal expected, actual
+
+    expected = array
+    actual = @league.worst_fans
+    assert_equal expected, actual
+  end 
 end
