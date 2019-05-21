3c1b805ac24eda98d49787fe2870ad1fbf9afabc
Sejin Kim <44950896+froydroyce@users.noreply.github.com>
2019-05-21 15:42:17 -0600

    Remove duplicated lines

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -21,23 +21,3 @@ attr_reader :games, :teams, :game_teams
     self.new(games, teams, game_teams)
   end
 end
-
-class StatTracker
-
-  def initialize(locations)
-    @game_path = game_path
-    @team_path =
-    @game_teams_path =
-    @location
-  end
-
-  def self.from_csv(locations)
-    CSV.table(locations)
-
-    # locations.each do |location|
-    end
-  end
-
-
-
-  # @games << Game.new(row)
