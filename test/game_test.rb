a6eff50ad2590c6c87585ad1e00ab96a93fe48de
Kelly Sandeson <kellysandeson@gmail.com>
2019-05-21 23:19:25 -0600

    Completion of game test file.

--- test/game_test.rb
+++ test/game_test.rb
@@ -7,54 +7,54 @@ require './lib/game_module'
 
 class GameTest < Minitest::Test
 
-  def setup
-    locations = {
-      games: './data/game_dummy.csv',
-      teams: './data/team_info_dummy.csv',
-      game_teams: './data/game_teams_stats_dummy.csv'
-    }
+    def setup
+      locations = {
+        games: './data/game_dummy.csv',
+        teams: './data/team_info_dummy.csv',
+        game_teams: './data/game_teams_stats_dummy.csv'
+      }
 
-    @stat_tracker = StatTracker.from_csv(locations)
+      @stat_tracker = StatTracker.from_csv(locations)
 
-  end
+    end
 
-  def test_highest_total_score
-    expected = 10
-    assert_equal expected, @stat_tracker.highest_total_score
-  end
+    def test_highest_total_score
+      expected = 10
+      assert_equal expected, @stat_tracker.highest_total_score
+    end
 
-#   def test_lowest_total_score
-#     expected = int
-#     assert_equal expected, @stat_tracker.lowest_total_score
-#   end
-#
-#   def test_biggest_blowout
-#     expected = int
-#     assert_equal expected, biggest_blowout
-#   end
-#
-#   def test_percentage_home_wins
-#     expected = float
-#     assert_equal expected, @stat_tracker.percentage_home_wins
-#   end
-#
-#   def test_percentage_visitor_wins
-#     expected = float
-#     assert_equal expected, @stat_tracker.percentage_visitor_wins
-#   end
-#
-#   def test_count_of_games_by_season
-#     expected = hash
-#     assert_equal expected, @stat_tracker.count_of_games_by_season
-#   end
-#
-#   def test_average_goals_per_game
-#     expected = float
-#     assert_equal expected, @stat_tracker.average_goals_per_game
-#   end
-#
-#   def test_average_goals_by_season
-#     expected = hash
-#     assert_equal expected, @stat_tracker.average_goals_by_season
-#   end
+    def test_lowest_total_score
+      expected = 1
+      assert_equal expected, @stat_tracker.lowest_total_score
+    end
+
+    def test_biggest_blowout
+      expected = 5
+      assert_equal expected, @stat_tracker.biggest_blowout
+    end
+
+    def test_percentage_home_wins
+      expected = 66.67
+      assert_equal expected, @stat_tracker.percentage_home_wins
+    end
+
+    def test_percentage_visitor_wins
+      expected = 33.33
+      assert_equal expected, @stat_tracker.percentage_visitor_wins
+    end
+
+    def test_count_of_games_by_season
+      expected = {"20122013" => 9}
+      assert_equal expected, @stat_tracker.count_of_games_by_season
+    end
+
+    def test_average_goals_per_game
+      expected = 5.11
+      assert_equal expected, @stat_tracker.average_goals_per_game
+    end
+  #
+    def test_average_goals_by_season
+      expected = {"20122013" => 5.11}
+      assert_equal expected, @stat_tracker.average_goals_by_season
+    end
 end
