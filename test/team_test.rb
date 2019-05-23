1dac377247f26e09d079462133e1e7f065873afe
Kelly Sandeson <kellysandeson@gmail.com>
2019-05-23 16:24:15 -0600

    Small changes for refactored code. Removals of unnecessary lines.
    Co-authored-by: 
    Co-Authored-By: jori peterson <joripeterson@gmail.com>
    Co-Authored-By: will thompson <wnt925@gmail.com>
    Co-Authored-By: sejin kim <froidroice@gmail.com>

--- test/team_test.rb
+++ test/team_test.rb
@@ -1,78 +1,37 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/team'
+require './lib/stat_tracker'
+
 
 class TeamTest < Minitest:: Test
-  def setup
-    @team = Team.new
-  end
 
-  def test_it_exists
-    expected = Team
-    actual = @team
-  end
+    def setup
+      locations = {
+        games: './test/data/game.csv',
+        teams: './test/data/team_info.csv',
+        game_teams: './test/data/game_teams_stats.csv'
+      }
+      @stat_tracker = StatTracker.from_csv(locations)
 
-  def test_team_attributes
-    expected = hash
-    actual = @team.team_info
-    assert_equal expected, actual
-  end
+    end
 
-  def test_highest_and_lowest_win_percentage
-    expected = int
-    actual = @team.best_season
-    assert_equal expected, actual
 
-    expected = int
-    actual = @team.worst_season
-    assert_equal expected, actual
-  end
+    def test_team_info
 
-  def test_average_win_percentage_all_games
-    expected = float
-    actual = @team.average_win_percentage
-    assert_equal expected, actual
-  end
+      expected = {
+          "team_id" => "1",
+          "franchise_id" => "23",
+          "short_name" => "New Jersey",
+          "team_name" => "Devils",
+          "abbreviation" => "NJD",
+          "link" => "/api/v1/teams/1" }
 
-  def test_most_and_fewest_goals_scored
-    expected = int
-    actual = @team.most_goals_scored
-    assert_equal expected, actual
+      assert_equal expected, @stat_tracker.team_info("1")
+    end
 
-    expected = int
-    actual = @team.fewest_goals_scored
-    assert_equal expected, actual
-  end
-
-  def test_opponent_with_lowest_and_highest_win_percentage
-    expected = string
-    actual = @team.favorite_opponent
-    assert_equal expected, actual
-
-    expected = string
-    actual = @team.rival
-    assert_equal expected, actual
-  end
-
-  def test_biggest_difference_in_scoring
-    actual = int
-    expected = @team.biggest_team_blowout
-    assert_equal expected, actual
-
-    actual = int
-    expected = @team.worst_loss
-    assert_equal expected, actual
-  end
-
-  def test_head_to_head_for_all_oponents
-    expected = hash
-    actual = @team.head_to_head
-    assert_equal expected, actual
-  end
-
-  def test_seasonal_summary
-    expected = hash
-    actual = @team.seasonal_summary
-    assert_equal expected, actual
-  end
+    def test_seasonal_summary
+      expected = {"20162017" => { => "2"}}
+      assert_equal expected, @stat_tracker.seasonal_summary("3")
+    end
 end
