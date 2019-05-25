3e7b5cab9f9736a792762862c9bf3770e4636b7b
Jori Peterson <joripeterson@gmail.com>
2019-05-24 21:25:41 -0600

    Refactor from merge conflict

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -3,42 +3,37 @@ require_relative 'game'
 require_relative 'team'
 require_relative 'game_team'
 require_relative 'game_module'
-<<<<<<< HEAD
 require_relative 'league_module'
+require_relative 'team_module'
 
 class StatTracker
-    include GameModule
-    include LeagueModule
-=======
-require_relative 'team_module_two'
+  include GameModule
+  include LeagueModule
+  include TeamModule
 
-class StatTracker
-    include GameModule
-    include TeamModule
->>>>>>> af729e6f76c3e7f9e0963542019827ed37a261a3
 
-    attr_reader :games, :teams, :game_teams
+  attr_reader :games, :teams, :game_teams
 
-    def initialize(games, teams, game_teams)
-      @games = games
-      @teams = teams
-      @game_teams = game_teams
+  def initialize(games, teams, game_teams)
+    @games = games
+    @teams = teams
+    @game_teams = game_teams
+  end
+
+  def self.from_csv(locations)
+    games = []
+    teams = []
+    game_teams = []
+
+    CSV.foreach(locations[:games], headers: true, header_converters: :symbol) do |row|
+      games << Game.new(row)
     end
-
-    def self.from_csv(locations)
-      games = []
-      teams = []
-      game_teams = []
-
-      CSV.foreach(locations[:games], headers: true, header_converters: :symbol) do |row|
-        games << Game.new(row)
-      end
-      CSV.foreach(locations[:teams], headers: true, header_converters: :symbol) do |row|
-        teams << Team.new(row)
-      end
-      CSV.foreach(locations[:game_teams], headers: true, header_converters: :symbol) do |row|
-        game_teams << GameTeam.new(row)
-      end
-      self.new(games, teams, game_teams)
+    CSV.foreach(locations[:teams], headers: true, header_converters: :symbol) do |row|
+      teams << Team.new(row)
     end
+    CSV.foreach(locations[:game_teams], headers: true, header_converters: :symbol) do |row|
+      game_teams << GameTeam.new(row)
+    end
+    self.new(games, teams, game_teams)
+  end
 end
