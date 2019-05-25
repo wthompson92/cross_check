82604536b1c760cfa75467763cc57851326777b5
Kelly Sandeson <kellysandeson@gmail.com>
2019-05-24 21:44:04 -0600

    Change expected to match larger data set

--- test/game_test.rb
+++ test/game_test.rb
@@ -18,7 +18,7 @@ class GameTest < Minitest::Test
     end
 
     def test_highest_total_score
-      expected = 11
+      expected = 12
       assert_equal expected, @stat_tracker.highest_total_score
     end
 
@@ -33,27 +33,27 @@ class GameTest < Minitest::Test
     end
 
     def test_percentage_home_wins
-      expected = 50.0
+      expected = 53.68
       assert_equal expected, @stat_tracker.percentage_home_wins
     end
 
     def test_percentage_visitor_wins
-      expected = 50.0
+      expected = 46.32
       assert_equal expected, @stat_tracker.percentage_visitor_wins
     end
 
     def test_count_of_games_by_season
-      expected = {"20122013" => 9, "20142015"=>11}
+      expected = {"20122013" => 44, "20152016" => 17, "20162017" => 17, "20172018" => 17}
       assert_equal expected, @stat_tracker.count_of_games_by_season
     end
 
     def test_average_goals_per_game
-      expected = 5.25
+      expected = 5.74
       assert_equal expected, @stat_tracker.average_goals_per_game
     end
 
     def test_average_goals_by_season
-      expected = {"20122013" => 5.11, "20142015"=>5.36}
+      expected = {"20122013" => 5.0, "20152016" => 6.24, "20162017" => 6.53, "20172018" => 6.35}
       assert_equal expected, @stat_tracker.average_goals_by_season
     end
 end
